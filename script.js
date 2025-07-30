557206b051aed175d3b7dfff2d4cd3dda7e7dc7d
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-07-30 12:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+function majorityElement(arr){
+	let count=0,ele=0;
+	for(let i=0;i<arr.length;i++){
+		if(count===0){
+			ele=arr[i];
+			count=1;
+		}else if(ele===arr[i]){
+			count++;
+		}else{
+			count--;
+		}
+	}
+
+	let majority = Math.floor(arr.length/2);
+	count=0;
+	for(let i=0;i<arr.length;i++){
+		if(ele===arr[i]) count++;
+	}
+	if(count>majority){
+		return ele;
+	}
+}
+const arr = [2,1,2];
+alert(majorityElement(arr));
